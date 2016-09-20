4f8dcc2b4b91f0750a7dcfb37668e593977317fe
Austin Steffes <asteffes16@gatech.edu>
2016-09-20 16:21:01 -0400

    Completing issue 22- check matching password confirmation. Added JS function checkPasswordsMatch(). Added JS script file to mentor.html.

--- bootstrap/dist/js/mentee.js
+++ bootstrap/dist/js/mentee.js
@@ -1,8 +1,23 @@
+// function to hide/show registration input fields depending on selection of student/notStudent radio buttons
 function showHideRegisterInfo() {
   var ifStudent = document.getElementById('studentRad');
   var ifNotStudent = document.getElementById('notStudentRad');
   studentRegForm.style.display = ifStudent.checked ? "block" : "none";
   levelRadios.style.display = ifStudent.checked ? "block" : "none";
   employedRegForm.style.display = ifNotStudent.checked ? "block" : "none";
+
+}
+
+// function to check if password and confirmation password match. gives alert if they do not.
+function checkPasswordsMatch() {
+  var pass1 = document.getElementById('inputPassword');
+  var pass2 = document.getElementById('confirmPassword');
+  var message = document.getElementById('confirmMessage');
+
+  if (pass1.value == pass2.value) {
+    message.innerHTML = "";
+  } else {
+    message.innerHTML = "Passwords do not match."
+  }
 
 }
