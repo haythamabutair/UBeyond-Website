bc4baa8ff5e2457480a1c1ec1a2f75a420cd0033
nolasaint <evanbailey122@gmail.com>
2016-10-05 22:09:23 -0400

    implemented welcome message generation

--- Matching/App_Data/Notification.cs
+++ Matching/App_Data/Notification.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Mail;
 using System.Web;
@@ -30,7 +31,7 @@ namespace Matching {
         /// <returns>
         /// <c>true</c> if the email(s) were sent without error, else <c>false</c>.
         /// </returns>
-        public static bool SendEmail(MailAddressCollection recipients, string message) {
+        public static bool SendEmail(MailAddressCollection recipients, string subject, string message) {
             MailMessage mail = new MailMessage();
             SmtpClient client = new SmtpClient();
             bool result = true;
@@ -45,6 +46,7 @@ namespace Matching {
 
             // Configure mail message
             mail.Body = message;
+            mail.Subject = subject;
             mail.From = new MailAddress(GMAIL_ACCT);
             foreach (MailAddress recipient in recipients) {
                 mail.To.Add(recipient);
@@ -60,6 +62,42 @@ namespace Matching {
             }
 
             return result;
+        }
+
+        /// <summary>
+        /// Generates an HTML-formatted welcome email to be sent upon new user registration.
+        /// </summary>
+        /// 
+        /// <param name="name">
+        /// The newly-registered user's name.
+        /// </param>
+        /// 
+        /// <param name="userID">
+        /// The newly-registered user's ID.
+        /// </param>
+        /// 
+        /// <remarks>
+        /// TODO: Check welcome.txt and put finishing touches on it, including fixing admin email and profile link.
+        /// </remarks>
+        /// 
+        /// <returns>
+        /// An HTML-formatted <c>string</c> to serve as the body of the email.
+        /// </returns>
+        public static string GenerateWelcomeEmail(string name, string userID) {
+            const string TEMPLATE_FILE = "templates/welcome.txt";
+            string message = null;
+
+            // Read template into a file
+            try {
+                using (StreamReader reader = File.OpenText(TEMPLATE_FILE)) {
+                    message = String.Format(reader.ReadToEnd(), name, userID);
+                }
+            }
+            catch (Exception ex) {
+                // TODO - could handle exceptions
+            }
+
+            return message;
         }
 
     }
