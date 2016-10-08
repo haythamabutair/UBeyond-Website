5abc5bb7a3884c88fb8f5606d0ced92e210e9048
tsolanki95 <tsolanki95@yahoo.in>
2016-10-08 18:59:36 -0400

    added Match object

---
+++ Matching/Models/Match.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Matching.Models
+{
+	public class Match
+	{
+        public string Mentee { get; set; }
+        public string Mentor { get; set; }
+	}
+}
