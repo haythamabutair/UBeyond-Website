27f75c02e0f7f3dc43742ef7bb860ece35d96452
tsolanki95 <tsolanki95@yahoo.in>
2016-10-08 12:29:11 -0400

    fixed merge conflicts

--- Matching/Models/User.cs
+++ Matching/Models/User.cs
@@ -5,38 +5,30 @@ using System.Web;
 
 namespace Matching.Models
 {
-    public class User
+    public abstract class User : Person
     {
-        public struct NameStruct
-        {
-            public string first;
-            public string last;
-            public string middle;
-            public string preferred;
-        }
-
-        public struct StudentInfoStruct
-        {
-            public string currentSchool;
-            public string expectedGraduationDate;
-            public string futurePlans;
-            public bool interestedInPostGrad;
-            public string major;
-            public string minor;
-        }
-
+        public string PathToHeadshot { get; set; }
+        public string PathToResume { get; set; }
         public string Bio { get; set; }
-        public string Birthdate { get; set; }
-        public string Email { get; set; }
-        public string FieldPreference { get; set; }
-        public string FirstMeetingDatePreference { get; set; }
-        public string Gender { get; set; }
-        public string GenderPreference { get; set; }
+        public DateTime PreferredStartDate { get; set; }
         public string LanguagePreference { get; set; }
-        public NameStruct Name { get; set; }
-        public string Phone { get; set; }
-        public StudentInfoStruct StudentInfo { get; set; }
+        public string GenderPreference { get; set; } //Male or Female
         public string UserType { get; set; }
+
+        //internal
         public bool IsAvailable { get; set; }
+
+        protected User(string firstName, string lastName, string middleInitial, string preferredName, string address, string phoneNumber, string email, string gender, List<string> languages, DateTime birthdate, string pathToHeadshot, string pathToResume, string bio, DateTime preferredStartDate, string languagePreference, string genderPreference, string userType) : base(firstName, lastName, middleInitial, preferredName, address, phoneNumber, email, gender, languages, birthdate)
+        {
+            this.PathToHeadshot = pathToHeadshot;
+            this.PathToResume = pathToResume;
+            this.Bio = bio;
+            this.PreferredStartDate = preferredStartDate;
+            this.LanguagePreference = languagePreference;
+            this.GenderPreference = genderPreference;
+            this.UserType = userType;
+
+            this.IsAvailable = true;
+        }
     }
 }
