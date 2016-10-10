1b62e1bc6d74ebf2b3cc22c45dde09b1f22a46b9
nolasaint <evanbailey122@gmail.com>
2016-10-10 18:26:35 -0400

    validation skeleton, only missing signature validation

--- Matching/App_Data/Authentication.cs
+++ Matching/App_Data/Authentication.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Web;
 using Microsoft.IdentityModel.Tokens;
 using System.Net;
 using System.IO;
@@ -13,12 +10,33 @@ namespace Matching {
 
     public static class Authentication {
 
-        private static string GetX509SecurityCertificates(string kid) {
+        /// <summary>
+        /// Fetches the public encryption key using the provided key index, from
+        /// the Google X509 API.
+        /// </summary>
+        /// 
+        /// <param name="kid">
+        /// Index of the public key to fetch.
+        /// </param>
+        /// 
+        /// <exception cref="ArgumentException">
+        /// Thrown when a key with the specified kid was not found at the API
+        /// endpoint.
+        /// </exception>
+        /// 
+        /// <returns>
+        /// The public encryption key, or <c>null</c> if not found.
+        /// </returns>
+        private static string GetX509SecurityCertificate(string kid) {
             string cert = null;
-            const string URL = "https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com";
 
-            // Fetch data from endpoint
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            // Create HTTP request to fetch current X509 public keys
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(
+                Properties.Settings.Default.GoogleX509ApiUrl
+            );
+
+            // TODO: Can cache these keys, and then check if they are expired
+
             try {
                 JObject jsonObject;
                 JToken jsonToken;
@@ -31,25 +49,38 @@ namespace Matching {
 
                 // Parse JSON data
                 if (jsonObject.TryGetValue(kid, out jsonToken)) {
+                    const string HEADER = "-----BEGIN CERTIFICATE-----";
+                    const string FOOTER = "-----END CERTIFICATE-----";
+
                     cert = jsonToken.ToString();
-                    // TODO fetch between -----BEGIN CERTIFICATE----- and -----END CERTIFICATE-----
+
+                    // Get rid of -----BEGIN/END CERTIFICATE-----
+                    int from = cert.IndexOf(HEADER) + HEADER.Length;
+                    int to = cert.LastIndexOf(FOOTER);
+
+                    cert = cert.Substring(from, to - from);
                 }
+                // Throw argument exception if key not found
                 else {
-                    // Failed because value not found
+                    throw new ArgumentException(
+                        "Public key with index " + kid + " was not found.",
+                        "kid"
+                    );
                 }
             }
             catch (Exception ex) {
-                //TODO
-                Console.WriteLine(ex.StackTrace.ToString());
+                // TODO: Could handle errors in future
             }
 
             return cert;
         }
 
-        public static string VerifyIdToken(string idToken) {
+        public static string VerifyIdToken(string idToken, DateTime receivedAt) {
             JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
-            JwtSecurityToken token = handler.ReadJwtToken(idToken);
-            //X509SecurityKey publicKey = new X509SecurityKey();
+            JwtSecurityToken jwtToken = handler.ReadJwtToken(idToken);
+            SecurityToken token;
+            string uid = null;
+
             /*
              * Note:
              * 
@@ -60,10 +91,59 @@ namespace Matching {
              * The worst part? This is the only library that provides payload verification which we ALSO need.
              * So yeah, godspeed to anyone who has to work with this.
              */
-            string cert = GetX509SecurityCertificates("87571901d6986cae36ceffeee6243c78190ec4ff");
-            Console.WriteLine("cert: {0}", cert);
-            return "test";
-        }
 
+            // Check that algorithm is RS256
+            if (Properties.Settings.Default.FirebaseTokenAlgorithm != jwtToken.Header.Alg) {
+                throw new ArgumentException(
+                    "Provided JWT token is not encrypted with " + Properties.Settings.Default.FirebaseTokenAlgorithm + ".",
+                    "idToken"
+                ) {
+                    Source = "GetX509SecurityCertificate"
+                };
+            }
+
+            // Set up validation parameters
+            TokenValidationParameters validationParams = new TokenValidationParameters() {
+                ValidAudience = Properties.Settings.Default.FirebaseProjectID,
+                ValidateAudience = true,
+
+                ValidIssuer = Properties.Settings.Default.FirebaseIssuerURL,
+                ValidateIssuer = true,
+
+                LifetimeValidator = (iat, exp, tok, val) => (
+                    (receivedAt >= iat) && (receivedAt < exp)
+                ),
+                ValidateLifetime = true
+            };
+
+            try {
+                // Check that kid corresponds to public key
+                string x509cert = GetX509SecurityCertificate(jwtToken.Header.Kid);
+
+                // TODO:
+                // Setup validationParams.SignatureValidator
+
+                // Validate token
+                handler.ValidateToken(idToken, validationParams, out token);
+
+                uid = ((JwtSecurityToken)token).Subject;
+            }
+            // Handle exception from GetX509SecurityCertificate
+            catch (ArgumentException ex) when ("GetX509SecurityCertificate" == ex.Source) {
+                throw new ArgumentException(
+                    "Provided JWT token has invalid kid parameter.",
+                    "idToken",
+                    ex
+                );
+            }
+            // TODO: handle specific exceptions
+            catch (Exception ex) {
+                Console.WriteLine("Exception: {0}", ex.GetType().ToString());
+                Console.WriteLine("Message:   {0}", ex.Message);
+                Console.WriteLine("Stacktrace:\n{0}", ex.StackTrace);
+            }
+
+            return uid;
+        }
     }
 }
