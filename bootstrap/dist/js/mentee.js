7b6a8fe50479b7e0e32bfd704e1082f9c2edda5a
Shannor Trotty <shannor.a.trotty@gmail.com>
2016-10-11 11:10:23 -0400

    Added variable to account for if information is provided.

--- bootstrap/dist/js/mentee.js
+++ bootstrap/dist/js/mentee.js
@@ -1,3 +1,7 @@
+//Global Var to allow onClick options to happen once info in inputed.
+var canUseBtn = false;
+
+
 // function to hide/show registration input fields depending on selection of student/notStudent radio buttons
 function showHideRegisterInfo() {
   var ifStudent = document.getElementById('studentRad');
@@ -58,8 +62,10 @@ $(function(){
 
       if (!allFilledOut) {
           $('#menteeContBtn').prop('disabled', true);
+          canUseBtn = false;
       } else {
           $('#menteeContBtn').prop('disabled',false);
+          canUseBtn = true;
       }
   });
 });
@@ -71,72 +77,74 @@ $(function(){
  */
 function onMenteeContBtnClick(caller) {
   event.preventDefault(); // Stop auto-navigation to href (chrome, firefox)
-
+  //Check to see that all required information is provided.
+  if(canUseBtn == true){
   // So we can navigate to the target HREF on success
-  var href = document.getElementById(caller.id).href;
+    var href = document.getElementById(caller.id).href;
 
-  var email = document.getElementById('inputEmail').value;
-  var password = document.getElementById('inputPassword').value;
+    var email = document.getElementById('inputEmail').value;
+    var password = document.getElementById('inputPassword').value;
 
-  // Attempt to register the specified email and password
-  Database.registerUser(email, password, function(success, response) {
-    if (success) {
-      // Update rest of mentee data
-      var genderObj = document.getElementById('gender');
+    // Attempt to register the specified email and password
+    Database.registerUser(email, password, function(success, response) {
+      if (success) {
+        // Update rest of mentee data
+        var genderObj = document.getElementById('gender');
 
-      var data = {
-        "userType": "mentee",
-        "email": email,
-        "birthdate": document.getElementById('bday').value,
-        "gender": genderObj.options[genderObj.selectedIndex].text,
-        "name": {
-          "first": document.getElementById('firstName').value,
-          "middle": document.getElementById('middleInitial').value,
-          "last": document.getElementById('lastName').value,
-          "preferred": document.getElementById('preferredName').value
-        },
-        "phone": document.getElementById('phoneNumber').value
-      };
-
-      // Student data
-      if (document.getElementById('studentRad').checked) {
-        data["studentInfo"] = {
-          "currentSchool": document.getElementById('currentSchool').value,
-          "major": document.getElementById('major').value,
-          "minor": document.getElementById('minor').value,
-          "futurePlans": document.getElementById('afterGrad').value,
-          "expectedGraduationDate": document.getElementById('studentGradDate').value,
-          "interestedInPostGrad": document.getElementById('postGradCheckBx').checked
+        var data = {
+          "userType": "mentee",
+          "email": email,
+          "birthdate": document.getElementById('bday').value,
+          "gender": genderObj.options[genderObj.selectedIndex].text,
+          "name": {
+            "first": document.getElementById('firstName').value,
+            "middle": document.getElementById('middleInitial').value,
+            "last": document.getElementById('lastName').value,
+            "preferred": document.getElementById('preferredName').value
+          },
+          "phone": document.getElementById('phoneNumber').value
         };
-      }
-      // Employer data
-      else {
-        data["employeeInfo"] = {
-          "employer": document.getElementById('currentEmployer').value,
-          "latestDegree": document.getElementById('latestDegree').value,
-          "latestSchool": document.getElementById('latestSchool').value,
-          "graduationDate": document.getElementById('employedGradDate').value,
-          "careerPlans": document.getElementById('careerPlan').value,
-          "careerGoals": document.getElementById('careerGoals').value
-        };
-      }
 
-      // Update database entry for newly-registered used and navigate to next page
-      Database.updateUserData(data, function(success, response) {
-        if(success) {
-          // Now navigate to target href
-          window.location.href = href;
+        // Student data
+        if (document.getElementById('studentRad').checked) {
+          data["studentInfo"] = {
+            "currentSchool": document.getElementById('currentSchool').value,
+            "major": document.getElementById('major').value,
+            "minor": document.getElementById('minor').value,
+            "futurePlans": document.getElementById('afterGrad').value,
+            "expectedGraduationDate": document.getElementById('studentGradDate').value,
+            "interestedInPostGrad": document.getElementById('postGradCheckBx').checked
+          };
         }
+        // Employer data
         else {
-          alert(response);
+          data["employeeInfo"] = {
+            "employer": document.getElementById('currentEmployer').value,
+            "latestDegree": document.getElementById('latestDegree').value,
+            "latestSchool": document.getElementById('latestSchool').value,
+            "graduationDate": document.getElementById('employedGradDate').value,
+            "careerPlans": document.getElementById('careerPlan').value,
+            "careerGoals": document.getElementById('careerGoals').value
+          };
         }
-      });
-    }
-    else {
-      alert(response);
-    }
-  });
 
-  // Stop auto-navigation to href (IE)
-  return false;
+        // Update database entry for newly-registered used and navigate to next page
+        Database.updateUserData(data, function(success, response) {
+          if(success) {
+            // Now navigate to target href
+            window.location.href = href;
+          }
+          else {
+            alert(response);
+          }
+        });
+      }
+      else {
+        alert(response);
+      }
+    });
+
+    // Stop auto-navigation to href (IE)
+    return false;
+  }
 }
