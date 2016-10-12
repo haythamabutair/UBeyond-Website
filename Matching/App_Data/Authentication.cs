b10798bf7f0040f1d30979d8b6b4ef3a7d504ae1
nolasaint <evanbailey122@gmail.com>
2016-10-12 16:33:41 -0400

    token validation functionality

--- Matching/App_Data/Authentication.cs
+++ Matching/App_Data/Authentication.cs
@@ -5,14 +5,57 @@ using System.Net;
 using System.IO;
 using System.Text;
 using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
 
 namespace Matching {
 
     public static class Authentication {
 
+        public enum ValidationResult {
+            VALIDATED = 0,
+
+            /// <summary>
+            /// Denotes the token was not encrypted with the RS256 algorithm.
+            /// </summary>
+            TOKEN_INVALID_ALG,
+
+            /// <summary>
+            /// Denotes the token has a kid that does not correspond to a valid public key.
+            /// </summary>
+            TOKEN_INVALID_KID,
+
+            /// <summary>
+            /// Denotes the token has expired.
+            /// </summary>
+            TOKEN_EXPIRED,
+
+            /// <summary>
+            /// Denotes the token was issued by a non-valid source.
+            /// </summary>
+            TOKEN_INVALID_ISSUER,
+            
+            /// <summary>
+            /// Denotes the token was targeting a non-valid audience.
+            /// </summary>
+            TOKEN_INVALID_AUDIENCE,
+
+            /// <summary>
+            /// Denotes that validation failed for an unknown or unexpected reason.
+            /// </summary>
+            UNKOWN_FAILURE
+        }
+
+        private static bool IsAlgorithmValid(string alg) {
+            return alg.Equals(Properties.Settings.Default.FirebaseTokenAlgorithm);
+        }
+
         /// <summary>
-        /// Fetches the public encryption key using the provided key index, from
-        /// the Google X509 API.
+        /// Checks for the existance of a public encryption key using the
+        /// provided key index, from the Google X509 API.
         /// </summary>
         /// 
         /// <param name="kid">
@@ -25,125 +68,134 @@ namespace Matching {
         /// </exception>
         /// 
         /// <returns>
-        /// The public encryption key, or <c>null</c> if not found.
+        /// <c>true</c> if the key was found, else <c>false</c>.
         /// </returns>
-        private static string GetX509SecurityCertificate(string kid) {
-            string cert = null;
-
+        private static bool IsSigningKeyValid(string kid) {
             // Create HTTP request to fetch current X509 public keys
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(
                 Properties.Settings.Default.GoogleX509ApiUrl
             );
 
-            // TODO: Can cache these keys, and then check if they are expired
+            JObject jsonObject;
+            JToken jsonToken;
 
-            try {
-                JObject jsonObject;
-                JToken jsonToken;
-
-                // Read data from response stream
-                using (Stream responseStream = request.GetResponse().GetResponseStream()) {
-                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
-                    jsonObject = JObject.Parse(reader.ReadToEnd());
-                }
-
-                // Parse JSON data
-                if (jsonObject.TryGetValue(kid, out jsonToken)) {
-                    const string HEADER = "-----BEGIN CERTIFICATE-----";
-                    const string FOOTER = "-----END CERTIFICATE-----";
-
-                    cert = jsonToken.ToString();
-
-                    // Get rid of -----BEGIN/END CERTIFICATE-----
-                    int from = cert.IndexOf(HEADER) + HEADER.Length;
-                    int to = cert.LastIndexOf(FOOTER);
-
-                    cert = cert.Substring(from, to - from);
-                }
-                // Throw argument exception if key not found
-                else {
-                    throw new ArgumentException(
-                        "Public key with index " + kid + " was not found.",
-                        "kid"
-                    );
-                }
-            }
-            catch (Exception ex) {
-                // TODO: Could handle errors in future
+            // Read data from response stream | TODO: Can cache these keys
+            using (Stream responseStream = request.GetResponse().GetResponseStream()) {
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                jsonObject = JObject.Parse(reader.ReadToEnd());
             }
 
-            return cert;
+            // Commented out since we are not returning the key anymore
+            //try {
+            //    // Parse JSON data
+            //    if (jsonObject.TryGetValue(kid, out jsonToken)) {
+            //        const string HEADER = "-----BEGIN CERTIFICATE-----";
+            //        const string FOOTER = "-----END CERTIFICATE-----";
+
+            //        cert = jsonToken.ToString();
+
+            //        // Get rid of -----BEGIN/END CERTIFICATE-----
+            //        int from = cert.IndexOf(HEADER) + HEADER.Length;
+            //        int to = cert.LastIndexOf(FOOTER);
+
+            //        cert = cert.Substring(from, to - from);
+            //    }
+            //    // Throw argument exception if key not found
+            //    else {
+            //        throw new ArgumentException(
+            //            "Public key with index " + kid + " was not found.",
+            //            "kid"
+            //        );
+            //    }
+            //}
+            //catch (Exception ex) {
+            //    // TODO: Could handle errors in future
+            //}
+
+            return jsonObject.TryGetValue(kid, out jsonToken);
         }
 
-        public static string VerifyIdToken(string idToken, DateTime receivedAt) {
-            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
-            JwtSecurityToken jwtToken = handler.ReadJwtToken(idToken);
-            SecurityToken token;
-            string uid = null;
+        private static bool IsTokenLifetimeValid(DateTime received, DateTime issued, DateTime expiry) {
+            return received >= issued && received < expiry;
+        }
 
-            /*
-             * Note:
-             * 
-             * Ok please don't use Microsoft.IdentityModel for firebase server-side token auth...
-             * So it works pretty well in general, albeit a little outdated.
-             * If you're trying to verify an RS256 signature of a token using a public key, however, you're basically screwed.
-             * There seems to be no way to natively do this within the library, even though it seems so trivial and commonplace.
-             * The worst part? This is the only library that provides payload verification which we ALSO need.
-             * So yeah, godspeed to anyone who has to work with this.
-             */
+        private static bool IsIssuerValid(string issuer) {
+            return issuer.Equals(Properties.Settings.Default.FirebaseIssuerURL);
+        }
+
+        private static bool IsAudienceValid(IList<string> audiences) {
+            bool valid = false;
+
+            // Check that the only audience is our firebase project
+            if (audiences.Count == 1) {
+                valid = audiences[0].Equals(Properties.Settings.Default.FirebaseProjectID);
+            }
+
+            return valid;
+        }
+
+        /// <summary>
+        /// Validates a received JWT idToken, and writes the token's uid to uid.
+        /// </summary>
+        /// 
+        /// <param name="idToken">
+        /// The JWT token to validate.
+        /// </param>
+        /// 
+        /// <param name="received">
+        /// The <c>DateTime</c> the token was received.
+        /// </param>
+        /// 
+        /// <param name="uid">
+        /// A <c>string</c> that will be populated with the token's uid if
+        /// validated.
+        /// </param>
+        /// 
+        /// <returns>
+        /// <c>ValidationResult.VALIDATED</c> if successful, else some
+        /// <c>ValidationResult</c> denoting what validation step failed.
+        /// </returns>
+        public static ValidationResult ValidateIdToken(string idToken, DateTime received, out string uid) {
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            JwtSecurityToken token = handler.ReadJwtToken(idToken);
+
+            // Convert issuing and expiration dates to DateTime
+            DateTime issued = new DateTime((long)token.Payload.Iat);
+            DateTime expiry = new DateTime((long)token.Payload.Exp);
+
+            // Return value
+            ValidationResult status = ValidationResult.VALIDATED;
+
+            // Set uid to null in case of failure
+            uid = null;
 
             // Check that algorithm is RS256
-            if (Properties.Settings.Default.FirebaseTokenAlgorithm != jwtToken.Header.Alg) {
-                throw new ArgumentException(
-                    "Provided JWT token is not encrypted with " + Properties.Settings.Default.FirebaseTokenAlgorithm + ".",
-                    "idToken"
-                ) {
-                    Source = "GetX509SecurityCertificate"
-                };
+            if (!IsAlgorithmValid(token.Header.Alg)) {
+                status = ValidationResult.TOKEN_INVALID_ALG;
+            }
+            // Check that the issuer signing key is valid
+            else if (!IsSigningKeyValid(token.Header.Kid)) {
+                status = ValidationResult.TOKEN_INVALID_KID;
+            }
+            // Check that token is not expired
+            else if (!IsTokenLifetimeValid(received, issued, expiry)) {
+                status = ValidationResult.TOKEN_EXPIRED;
+            }
+            // Validate issuer
+            else if (!IsIssuerValid(token.Payload.Iss)) {
+                status = ValidationResult.TOKEN_INVALID_ISSUER;
+            }
+            // Validate audience
+            else if (!IsAudienceValid(token.Payload.Aud)) {
+                status = ValidationResult.TOKEN_INVALID_AUDIENCE;
+            }
+            // Token has been validated
+            else {
+                uid = token.Payload.Sub;
+                status = ValidationResult.VALIDATED;
             }
 
-            // Set up validation parameters
-            TokenValidationParameters validationParams = new TokenValidationParameters() {
-                ValidAudience = Properties.Settings.Default.FirebaseProjectID,
-                ValidateAudience = true,
-
-                ValidIssuer = Properties.Settings.Default.FirebaseIssuerURL,
-                ValidateIssuer = true,
-
-                LifetimeValidator = (iat, exp, tok, val) => (
-                    (receivedAt >= iat) && (receivedAt < exp)
-                ),
-                ValidateLifetime = true
-            };
-
-            try {
-                // Check that kid corresponds to public key
-                string x509cert = GetX509SecurityCertificate(jwtToken.Header.Kid);
-
-                // TODO:
-                // Setup validationParams.SignatureValidator
-
-                // Validate token
-                handler.ValidateToken(idToken, validationParams, out token);
-
-                uid = ((JwtSecurityToken)token).Subject;
-            }
-            // Handle exception from GetX509SecurityCertificate
-            catch (ArgumentException ex) when ("GetX509SecurityCertificate" == ex.Source) {
-                throw new ArgumentException(
-                    "Provided JWT token has invalid kid parameter.",
-                    "idToken",
-                    ex
-                );
-            }
-            // TODO: handle specific exceptions
-            catch (Exception ex) {
-                Console.WriteLine("Exception: {0}", ex.GetType().ToString());
-                Console.WriteLine("Message:   {0}", ex.Message);
-                Console.WriteLine("Stacktrace:\n{0}", ex.StackTrace);
-            }
-
-            return uid;
+            return status;
         }
     }
 }
