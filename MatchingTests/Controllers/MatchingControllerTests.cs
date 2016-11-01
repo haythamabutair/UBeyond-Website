e9c0335a92b043dd2bb2ef3128f8834b800debf4
tsolanki95 <tsolanki95@yahoo.in>
2016-10-31 23:52:00 -0400

    a unit test

---
+++ MatchingTests/Controllers/MatchingControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Matching.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matching.Controllers.Tests
+{
+    [TestClass()]
+    public class MatchingControllerTests
+    {
+        [TestMethod()]
+        public void MatchMentorTest()
+        {
+            MatchingController controller = new MatchingController();
+            controller.MatchMentor("LBSd2c8BuqVQq4osY2H9oKv5Gnv2");
+        }
+    }
+}
