a58e0b40e0119d2ce89076b6216aba7a69c7c165
Zack Littke-Smith <zdizzler@yahoo.com>
2016-11-15 16:57:31 -0500

    Added reference object and updated mentor form to include minimal info about references

---
+++ Matching/Models/Reference.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Matching.Models
+{
+    //class for mentor references
+    public class Reference
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Relationship { get; set; }
+
+        public Reference(string FirstName, string LastName, string PhoneNumber, string Email, string Relationship)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.PhoneNumber = PhoneNumber;
+            this.Email = Email;
+            this.Relationship = Relationship;
+        }
+    }
+}
