a569989276ec3a97caf43a0a0b0c4ddcfa6cd69b
Shannor Trotty <shannor.a.trotty@gmail.com>
2016-11-28 13:46:19 -0500

    Got admin page up and working.

--- bootstrap/dist/js/admin.js
+++ bootstrap/dist/js/admin.js
@@ -1,18 +1,11 @@
-// TODO: Fucntion to confirm a mentor/mentee pair
-// TODO: Function to dynamically create all mentor/mentee Pairs
-// TODO: Method to get all information from Firebase.
-
-//Gloabal arrays to hold the matching pairs, share the same index
-var listOfMentees =  [];
-var listOfMentors = [];
+//Count of how many Mentee's and mentor's
 var menteeCount = 0;
 var mentorCount = 0;
 //LanguagePreference & MenteeLevelPrefence are arrays from Firebase
-var listOfMentorKeys = ["FirstName","LastName","Birthdate","Gender", "Languages", "GenderPreference","Bio",
-"LanguagePreference", "PreferredStartDate", "YearsOfExperience", "FieldOfExpertise", "MenteeLevelPreference","Employer", "Strenghts"]
-
-var listOfMenteeKeys =  ["FirstName","LastName","Birthdate","Gender", "Languages", "GenderPreference","Bio",
-"LanguagePreference", "PreferredStartDate", "MenteeSkills", "FieldOfExpertise", "FieldPreference", "EmploymentStatus"]
+var listOfMentorKeys = ["First Name","Last Name","Birthdate","Gender", "Languages", "Gender Preference","Bio",
+"Language Preference", "Preferred StartDate", "Years Of Experience", "Field Of Expertise", "Mentee Level Preference","Employer", "Strenghts"];
+var listOfMenteeKeys =  ["First Name","Last Name","Birthdate","Gender", "Languages", "Gender Preference","Bio",
+"Language Preference", "Preferred StartDate", "Mentee Skills", "Field Of Expertise", "Field Preference", "Employment Status"];
 
 // Sign-in to use Firebase, Will change after Login Screen Linked 
 $(function(){
@@ -43,9 +36,10 @@ function getMatches(){
     var count = 0;
     menteeMatches.on('value',function(snapshot){
         if(snapshot.val() != null){
-            //Loop through each pair 
+            //Loop through each pair of matches  
             snapshot.forEach(function(childSnapshot){
-                makeContainters(count)
+                //Addes containers for each mentor/mentee match 
+                makeContainters(count);
                 count++;
                 var mentee = childSnapshot.key;
                 var mentor;  
@@ -53,21 +47,26 @@ function getMatches(){
                     mentor = childSnapshot.val()[x]["User"];
                 }
 
-                // Needed to get information about Mentor and Mentee
+                // Firebase calls to get Specific information about mentor and mentee
                 var mentorInfo = database.ref("Mentor/" + mentor);
                 var menteeInfo = database.ref("Mentee/" + mentee);
-                var studentInfo = database.ref("StudentInfo/" + mentee);
 
                 //Get Mentee Information 
                 menteeInfo.once('value',function(snapshot){
-                    //Need to get student Info, if Student 
+                    //Can be a student or employed
                     if(snapshot.val().EmploymentStatus == "student"){
+                        //Firabse call to get student information
+                        var studentInfo = database.ref("StudentInfo/" + mentee);
                         studentInfo.once('value', function(childSnapshot){
                             createMenteeObject(snapshot.val(), childSnapshot.val());
                         });
 
                     }else{
-                        // TODO: Check if Mentee is a Employee/Worker
+                        //Firebase call to get Employed Information
+                        var employedInfo = database.ref("EmployeeInfo/" + mentee);
+                        employedInfo.once('value', function(childSnapshot){
+                            createMenteeObject(snapshot.val(), childSnapshot.val());
+                        });
                     }
                 }); //End MenteeInfo Method 
 
@@ -84,43 +83,73 @@ function getMatches(){
 
 }//End of getMatches()
 
-//dynamically makes the container to 
+//dynamically makes the container to hold the information of mentors and metee matching 
 function makeContainters(index){
     //Main container to hold all the information 
     var container = document.getElementById("containerMM");
     var fluidContainer = document.createElement("div");
-    var rowDiv = document.createElement("div");
-    fluidContainer.setAttribute("class","container-fluid");
-    fluidContainer.setAttribute("id","mentee-mentor-comparision" + index);
-    rowDiv.setAttribute("clsss","row");
-    rowDiv.setAttribute("id","row-comparision"+index)
+    var informationDiv = document.createElement("div");
+    //Make div's for mentee and mentor since Firebase calls are async don't know when they will return
+    var menteeDiv = document.createElement("div");
+    var mentorDiv = document.createElement("div");
+    //Function to creat buttons 
     var btnDiv = createButtons(index);
-    fluidContainer.appendChild(rowDiv);
-
+    
+    fluidContainer.setAttribute("class","row well");
+    fluidContainer.setAttribute("id","mentee-mentor-comparision" + index);
+    informationDiv.setAttribute("clsss","col-md-12");
+    //Need to handle floating div problem 
+    informationDiv.setAttribute("style","overflow: hidden;");
+    menteeDiv.setAttribute("class","col-md-6");
+    menteeDiv.setAttribute("id","mentee"+ index);
+    mentorDiv.setAttribute("class","col-md-6");
+    mentorDiv.setAttribute("id","mentor"+index);
+    
+    informationDiv.appendChild(menteeDiv);
+    informationDiv.appendChild(mentorDiv);
+    fluidContainer.appendChild(informationDiv);
     fluidContainer.appendChild(btnDiv);
     container.appendChild(fluidContainer);
 }
 
 function createButtons(index){
 
+    //Create html objects
     var btnDiv = document.createElement("div");
-    btnDiv.setAttribute("class", "row");
-    var colDiv = document.createElement("div");
-    colDiv.setAttribute("class","col-md-6");
-    btnDiv.appendChild(colDiv);
     var accceptBtn = document.createElement("button");
     var declineBtn = document.createElement("button");
+
+    //Set attributes
+    btnDiv.setAttribute("class", 'text-center')
     accceptBtn.setAttribute("type", "button");
-    accceptBtn.setAttribute("class", "btn btn-primary group" + index);
+    accceptBtn.setAttribute("class", "btn btn-lg btn-primary");
     accceptBtn.innerText = "Accept"
     declineBtn.setAttribute("type", "button");
-    declineBtn.setAttribute("class", "btn btn-danger group" + index);
+    declineBtn.setAttribute("class", "btn btn-lg btn-danger");
     declineBtn.innerText = "Reject"
-    colDiv.appendChild(accceptBtn);
-    colDiv.appendChild(declineBtn); 
-     
+
+    //Set function to send information back to firebase and remove from html
+    accceptBtn.setAttribute('onclick','onAccept(' + index +')');
+    declineBtn.setAttribute('onclick','onReject(' + index + ')');
+
+    btnDiv.appendChild(accceptBtn);
+    btnDiv.appendChild(declineBtn); 
     return btnDiv;
 }
+
+
+function onAccept(index){
+    // remove div with mentee and mentor
+    $("#mentee-mentor-comparision" + index).remove();
+    console.log(menteeKeys)
+    //Send information back to firebase
+}
+
+function onReject(index){
+    $("#mentee-mentor-comparision" + index).remove();
+
+}
+
 
 // Helper method to create elements 
 function createElements(elementType, inText){
@@ -129,25 +158,24 @@ function createElements(elementType, inText){
     return temp;
 }
 
+//Code to dynamically make Mentee HTML Objects, Needs addon because of changing data
 function createMenteeObject(coreSnapShot, addOnSnapShot){
-    var mainContainer = document.getElementById("row-comparision" + menteeCount);
+    var mainContainer = document.getElementById("mentee" + menteeCount);
     menteeCount++;
 
-    var mainDiv = document.createElement("div");
-    mainDiv.setAttribute("class","col-md-6 mentee")
-
     var dl = document.createElement("dl");
-    mainDiv.appendChild(dl);
+    dl.setAttribute("class", "dl-horizontal");
+    mainContainer.appendChild(dl);
 
     //Add Core Mentee Info 
     listOfMenteeKeys.forEach(function(value){
         var tempdt = createElements("dt",value)
-        var tempdd = createElements("dd",coreSnapShot[value])
+        var tempdd = createElements("dd",coreSnapShot[value.replace(/\s/g, '')])
         dl.appendChild(tempdt);
         dl.appendChild(tempdd);
     });
     
-    //Specific add on data, Want all of it 
+    //Specific add on data ie. Worker or student
     for (var key in addOnSnapShot){
         var value = addOnSnapShot[key];
         var tempdt = createElements("dt",key);
@@ -155,29 +183,24 @@ function createMenteeObject(coreSnapShot, addOnSnapShot){
         dl.appendChild(tempdt);
         dl.appendChild(tempdd);
     }
-
-    mainContainer.appendChild(mainDiv);
 }
 
-
+//Code to dynamically make Mentor HTML and add it to the page. 
 function createMentorObject(mentorSnapshot){
-    var mainContainer = document.getElementById("row-comparision" + mentorCount);
+    var mainContainer = document.getElementById("mentor" + mentorCount);
     mentorCount++;
 
-    var mainDiv = document.createElement("div");
-    mainDiv.setAttribute("class","col-md-6 mentor")
     var dl = document.createElement("dl");
-    mainDiv.appendChild(dl);
+    dl.setAttribute("class", "dl-horizontal");
+    mainContainer.appendChild(dl);
 
 
     //Loop through the Keys and creates an element for each 
     listOfMentorKeys.forEach(function(value){
         var tempdt = createElements("dt",value)
-        var tempdd = createElements("dd",mentorSnapshot[value])
+        var tempdd = createElements("dd",mentorSnapshot[value.replace(/\s/g, '')])
         dl.appendChild(tempdt);
         dl.appendChild(tempdd);
     });
-    
-    mainContainer.appendChild(mainDiv);
 }
 
